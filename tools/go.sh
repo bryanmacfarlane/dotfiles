b0fd1c08310d2a870e9343d440ff732530d36d37
Bryan MacFarlane <bryanmacf@gmail.com>
2021-04-17 12:44:25 -0400

    go fix

--- tools/go.sh
+++ tools/go.sh
@@ -1,11 +1,8 @@
 go_path="/usr/local/go/bin"
-go_in_path=`which go`
 
-if [ -f  ${go_path} ]; then
-    echo go installed ...
-    
-    if ! [ -f ${go_in_path} ]; then 
-        echo found ${go_in_path}
+if [ -d  ${go_path} ]; then    
+    if [ -x "$(command -v go)" ]; then 
+        echo found "$(command -v go)"
     else 
         echo "Adding go to PATH"
         export PATH=$PATH:${go_path}
